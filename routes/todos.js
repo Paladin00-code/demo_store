2884c48d9f4bc3267af19deaebfc98627977b19e
harshperson <harshperson@gmail.com>
2021-01-03 02:06:26 +0200

    finish

--- routes/todos.js
+++ routes/todos.js
@@ -1,22 +1,40 @@
-const {Router}=require('express')
+const { Router } = require('express')
 const Todo = require('../models/todo')
 const router = Router()
 
-router.get('/', async (req,res) =>{
-    const todos  = await Todo.find({})
+router.get('/', async (req, res) => {
+  const todos = await todo.find({}).lean()
 
-    res.render('index', {
-        title: 'Toto list',
-        isIndex: true,
-        todos
-    })
+  res.render('index', {
+    title: 'Todos list',
+    isIndex: true,
+    todos
+  })
 })
 
 router.get('/create', (req, res) => {
-    res.render('create', {
-        title: 'Create todo',
-        isCreate: true
-    })
+  res.render('create', {
+    title: 'Create todo',
+    isCreate: true
+  })
+})
+
+router.post('/create', async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title
+  })
+
+  await todo.save()
+  res.redirect('/')
+})
+
+router.post('/complete', async (req, res) => {
+  const todo = await todo.findById(req.body.id)
+
+  todo.completed = !!req.body.completed
+  await todo.save()
+
+  res.redirect('/')
 })
 
 module.exports = router
